61aaab2cd0bc61debd37087bc504772e8f78c678
Swathi <swathiashok94@gmail.com>
2024-04-11 10:31:43 +0530

    first commit

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Styles from '../sidebar/sidebar.module.css'
 import { useLocation, useNavigate } from 'react-router-dom';
-import { ActiveDoccument, Doccument, DropDown, DropUp } from '../Svg';
+import { ActiveDoccument, Doccument, DropDown, DropUp, Report } from '../Svg';
 
 const Sidebar = ({ children }) => {
     const navigate = useNavigate();
@@ -49,7 +49,7 @@ const Sidebar = ({ children }) => {
             User
         </p>
     </div>
-    <div className={path === 'vendorManagement' ? `${Styles.menuActive}` : `${Styles.menuStyle}`} style={{marginTop:30}} onClick={() => navigate('/vendorManagement')}>
+    <div className={path === 'vendorManagement' ? `${Styles.menuActive}` : `${Styles.menuStyle}`} onClick={() => navigate('/vendorManagement')}>
         <div className={Styles.width}>
             {path === 'vendorManagement' ? (
             <ActiveDoccument/>
@@ -59,7 +59,7 @@ const Sidebar = ({ children }) => {
             </p>
         </div>
         {path === 'vendorManagement' ? (
-            <div style={{marginTop:10,marginRight:5}}>
+            <div>
                 <DropDown/>
             </div>
             ): <DropUp/>}
@@ -123,7 +123,7 @@ const Sidebar = ({ children }) => {
     <div className={path === 'report' ? `${Styles.menuActive}` : `${Styles.menuStyle}`} style={{marginTop:30}} onClick={() => navigate('/report')}>
         <div className={Styles.width}>
             {path === 'report' ? (
-            <ActiveDoccument/>
+            <Report/>
             ): <Doccument/>}
             <p className={Styles.dashboardText}>
                 Reports
