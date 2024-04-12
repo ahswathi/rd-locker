5af4292862365bba992ac71042af6f573c822682
Swathi <swathiashok94@gmail.com>
2024-04-12 10:33:11 +0530

    testing

--- src/receipt/receipt.js
+++ src/receipt/receipt.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from '../receipt/receipt.module.css'
+import { Export } from '../Svg'
 
 const Receipt = () => {
   return (
@@ -9,8 +10,17 @@ const Receipt = () => {
         </div>
         <div className={styles.row2}>
             <div className={styles.row2}>
-            Hello Charles Hall, <br/>
-This is the receipt for a payment of INR 268.00 you made to Relik - Admin Dashboard Demo.
+                Hello Charles Hall, <br/>
+                This is the receipt for a payment of INR 268.00 you made to Relik - Admin Dashboard Demo.
+            </div>
+            <div className={styles.row3}>
+                    <div className={styles.export}>
+                        <Export/> <span>Download</span>
+                    </div>
+                    <div className={styles.export}>
+                        <Export/> <span>Export</span>
+                    </div>
+                
             </div>
         </div>
         <div className={styles.row3}>
